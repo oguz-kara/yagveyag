6d75f4567b4ee7a40f87d48fdc3d169cf0673b18
Oguz <dev.oguz.kara@gmail.com>
2023-09-20 02:00:59 +0300

    initial

---
+++ src/api/info-type/controllers/info-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * info-type controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::info-type.info-type');
---
+++ src/api/info-type/routes/info-type.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * info-type router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::info-type.info-type');
